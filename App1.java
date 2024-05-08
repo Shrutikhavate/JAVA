e86b258b9350174e96c65b04b62fbd9eee51e6d9
Shrutikhavate <165253283+Shrutikhavate@users.noreply.github.com>
2024-05-08 22:16:55 +0530

    Add files via upload

---
+++ App1.java
@@ -0,0 +1,13 @@
+public class App1
+{
+    public static void main(String[]args)
+    {
+        int i=0;
+        while(i<=5)
+        {
+            System.out.println("Welcome");
+            i++;
+        }
+        return;
+    }
+}
